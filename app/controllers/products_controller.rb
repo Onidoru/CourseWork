339c35ab727e8e5d0ca834a7ffe1ed9747d7bb3e
Onidoru <onidoru@yahoo.com>
2017-05-01 14:04:41 +0300

    ActionCable fix & Added users

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -44,8 +44,8 @@ class ProductsController < ApplicationController
         format.json { render :show, status: :ok, location: @product }
 
         @products = Product.all
-        ApplicationCable.server.broadcast 'products',
-                                          html: render_to_string('store/index', layout: false)
+        ActionCable.server.broadcast 'products',
+                                     html: render_to_string('store/index', layout: false)
       else
         format.html { render :edit }
         format.json { render json: @product.errors, status: :unprocessable_entity }
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require_relative 'boot'
-
 require 'rails/all'
+require 'action_cable'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,10 +11,10 @@ module Store
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.autoload_paths += %W[#{config.root}/app/channels]
 
     config.generators do |g|
       g.template_engine :slim
     end
-
   end
 end
